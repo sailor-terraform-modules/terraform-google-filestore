ed6264da5be8c3cb75f003c7e1b283c72ed0150c
Vijay-bootlabs <vijay@bootlabstech.com>
2023-07-12 16:10:15 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "google_project_service" "filestoreapi" {
+  project = var.project_id
+  service = "file.googleapis.com"
+}
+resource "google_filestore_instance" "instance" {
+  depends_on = [google_project_service.filestoreapi]
+  name       = var.name
+  zone       = var.zone
+  tier       = var.tier
+  project    = var.project_id
+  file_shares {
+    capacity_gb = var.capacity_gb
+    name        = var.file_shares_name
+  }
+  networks {
+    network           = var.network
+    modes             = var.modes
+    reserved_ip_range = var.reserved_ip_range
+    connect_mode      = "PRIVATE_SERVICE_ACCESS"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "name" {
+  type        = string
+  description = "the name of the filestore"
+}
+variable "zone" {
+  type        = string
+  description = "the zone of deployment of filestore"
+}
+variable "tier" {
+  type        = string
+  description = "the usage tier of filestore"
+}
+variable "project_id" {
+  type        = string
+  description = "the id of project in which filestore will be created"
+}
+variable "capacity_gb" {
+  type        = number
+  description = " File share capacity in GiB. This must be at least 1024 GiB for the standard tier"
+}
+variable "file_shares_name" {
+  type        = string
+  description = "The name of the fileshare (16 characters or less)"
+}
+variable "network" {
+  type        = string
+  description = "the network that the filestore will use."
+}
+variable "modes" {
+  type        = list(string)
+  description = " IP versions for which the instance has IP addresses assigned. VEach of values may be ADDRESS_MODE_UNSPECIFIED, MODE_IPV4, and MODE_IPV6"
+  default     = ["MODE_IPV4"]
+}
+
+variable "reserved_ip_range" {
+  type        = string
+  description = "A /29 CIDR block that identifies the range of IP addresses reserved for this instance."
+}
